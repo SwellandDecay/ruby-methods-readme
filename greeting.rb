87e08c0ece2e06e53e44885cbcde1e7385791d71
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:44:05 -0400

    Archive Flatiron code.

---
+++ greeting.rb
@@ -0,0 +1,14 @@
+def greeting #Method Signature
+    puts "Hello World" #Method Body
+end #Method Closing
+
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
